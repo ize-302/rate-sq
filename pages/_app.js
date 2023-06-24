fd0b144051e92b086344e13c0a3de68286b43f0e
ize-send <adavize.hassan@send.ng>
2023-06-24 22:57:35 +0100

    setup supabase

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "@/styles/globals.css";
+import "@/styles/custom.scss";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }) {
+  const [supabase] = React.useState(() => createPagesBrowserClient());
 
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <MantineProvider
+        theme={{
+          fontFamily: "Inter, sans-serif",
+        }}
+        withNormalizeCSS
+      >
+        <Notifications position="top-right" containerWidth={350} />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SessionContextProvider>
   );
 }
