b61a765877dcb2d84ae9c23a51a89c6283df1c05
ize-send <adavize.hassan@send.ng>
2023-06-26 22:50:53 +0100

    update test

--- __tests__/pages/api/v1/search/composer.test.js
+++ __tests__/pages/api/v1/search/composer.test.js
@@ -2,7 +2,7 @@ import { createMocks } from "node-mocks-http";
 import composerSearchHandler from '@/pages/api/v1/search/composer';
 
 describe('/api/v1/search/composer', () => {
-  it('Returns 200 status code and array of search results', async () => {
+  it('Returns 200 status code and array of search results', () => {
     const { req, res } = createMocks({
       method: 'GET',
       query: {
