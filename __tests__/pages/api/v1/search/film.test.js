211d5f7c9648cf52aefea13d908611f2f0a3622b
ize-send <adavize.hassan@send.ng>
2023-06-26 22:47:52 +0100

    update test

--- __tests__/pages/api/v1/search/film.test.js
+++ __tests__/pages/api/v1/search/film.test.js
@@ -2,14 +2,14 @@ import { createMocks } from "node-mocks-http";
 import filmSearchHandler from '@/pages/api/v1/search/film';
 
 describe('/api/v1/search/film', () => {
-  it('Returns 200 status code and array of search results', async () => {
+  it('Returns 200 status code and array of search results', () => {
     const { req, res } = createMocks({
       method: 'GET',
       query: {
         query: 'game of thrones'
       }
     });
-    await filmSearchHandler(req, res);
+    filmSearchHandler(req, res);
     expect(res._getStatusCode()).toBe(200);
   })
 });
