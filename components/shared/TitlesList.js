696164f452875d0cfab098d4c6f53e67f2ec8dba
ize-send <adavize.hassan@send.ng>
2023-07-06 12:25:20 +0100

    fix image bleed

--- components/shared/TitlesList.js
+++ components/shared/TitlesList.js
@@ -22,7 +22,7 @@ const Item = ({ item, setdraweritem }) => {
               )}
             </div>
           </div>
-          <Avatar className='rounded-lg bg-secondary h-[340px] relative w-full' src={item?.poster_path ? `https://image.tmdb.org/t/p/original/${item?.poster_path}` : ''} />
+          <Avatar className='rounded-xl bg-secondary h-[340px] relative w-full' src={item?.poster_path ? `https://image.tmdb.org/t/p/original/${item?.poster_path}` : ''} />
         </LazyLoad>
         <Link href={`/tv/${item.id}`} className='font-semibold mt-2'>{item.name} opening title</Link>
       </div>
