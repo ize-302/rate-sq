cc68c4f97d6b8f393d95ae95c9c1d906a312cbba
ize-send <adavize.hassan@send.ng>
2023-07-07 23:35:56 +0100

    make acount page inaccesible to guest users

--- pages/account/index.js
+++ pages/account/index.js
@@ -9,24 +9,36 @@ export default function AccountPage() {
 
   const form = useForm({
     initialValues: {
-      display_name: user?.display_name,
+      display_name: '',
+      email: '',
     },
     validate: {
       display_name: (value) => value ? null : 'Name is required',
     },
   });
 
+  React.useEffect(() => {
+    form.setFieldValue('display_name', user?.display_name);
+    form.setFieldValue('email', user?.email);
+  }, [user])
+
   return (
     <AccountLayout title='My account'>
-      <form className='max-w-md' onSubmit={form.onSubmit((values) => updateProfile(values))}>
+      <form className='max-w-md flex flex-col gap-4' onSubmit={form.onSubmit((values) => updateProfile(values))}>
         <TextInput
           placeholder="Display name"
           label="Display name"
           withAsterisk
-          disabled
           {...form.getInputProps('display_name')}
         />
-        <Button disabled loading={loading} type='submit' className='mt-4 border-0 bg-secondary hover:bg-secondary'>Update</Button>
+        <TextInput
+          placeholder="Email address"
+          label="Email address"
+          withAsterisk
+          disabled
+          {...form.getInputProps('email')}
+        />
+        <Button loading={loading} type='submit' className='mt-4 border-0 bg-secondary hover:bg-secondary'>Update</Button>
       </form>
     </AccountLayout>
   )
